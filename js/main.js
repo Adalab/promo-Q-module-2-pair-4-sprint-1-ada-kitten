f58de1b4892a89f175fdd676344e29e01ac9cda0
modehans <monicaglezsanch@gmail.com>
2022-06-28 11:26:50 +0200

    create menu collapsed interective

--- js/main.js
+++ js/main.js
@@ -1,15 +1,41 @@
 'use strict'
 
-const CollapsedForm = document.querySelector('.js-new-form');
 
-let kittenImage = 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
 
-let kittenName = 'Anastacio';
+const kittenImage1 = 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
 
-let kittenDesc =
+const kittenImage2 = 'https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg';
+
+const kittenImage3 = 'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg';
+
+const name1 = 'Anastacio';
+
+const kittenName1 = name1.toUpperCase();
+
+
+const name2 = 'Fiona';
+
+const kittenName2 = name2.toUpperCase();
+
+const name3 = 'Cielo';
+
+
+const kittenName3 = name3.toUpperCase();
+
+const kittenDesc1 =
   'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
 
-let kittenRace = 'British Shorthair';
+  const kittenDesc2 =
+  'b';
+
+  const kittenDesc3 =
+  'c';
+
+const kittenRace1 = 'British Shorthair';
+
+const kittenRace2 = 'Raza 2';
+
+const kittenRace3 = 'Raza 3';
 
 
 
@@ -17,48 +43,94 @@ let kittenRace = 'British Shorthair';
 
 let KittenData = document.querySelector('.js-list');
 
-const Cat1  = `<li class="card">
+const Cat1  = `<li class="card js_cat1">
 <article>
   <img
     class="card_img"
-    src=${kittenImage}
+    src=${kittenImage1}
     alt="gatito"
   />
-  <h3 class="card_title">${kittenName}</h3>
-  <h4 class="card_race">${kittenRace}</h4>
+  <h3 class="card_title">${kittenName1}</h3>
+  <h4 class="card_race">${kittenRace1}</h4>
   <p class="card_description">
-${kittenDesc}
+${kittenDesc1}
   </p>
 </article>
 </li>`;
 
 
-const Cat2 = `<li class="card">
+const Cat2 = `<li class="card js_cat2">
 <img
   class="card_img"
-  src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg"
+  src="${kittenImage2}"
   alt="gatito"
 />
-<h3 class="card_title">Fiona</h3>
-<h4 class="card_race">British Shorthair</h4>
+<h3 class="card_title">${kittenName2}</h3>
+<h4 class="card_race">${kittenRace2}</h4>
 <p class="card_description">
-  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
-  Es una maravilla acariciarle!
+${kittenDesc2}
 </p>
 </li>`;
 
-const Cat3 = `<li class="card">
+const Cat3 = `<li class="card js_cat3">
 <img
   class="card_img"
-  src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg"
+  src="${kittenImage3}"
   alt="gatito"
 />
-<h3 class="card_title">Cielo</h3>
-<h4 class="card_race">British Shorthair</h4>
+<h3 class="card_title">${kittenName3}</h3>
+<h4 class="card_race">${kittenRace3}</h4>
 <p class="card_description">
-  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
-  Es una maravilla acariciarle!
+${kittenDesc3}
 </p>
 </li>`;
 
 KittenData.innerHTML = `${Cat1} + ${Cat2} + ${Cat3}`;
+
+const inputSearchDesc = document.querySelector('.js_in_search_desc');
+
+/* inputSearchDesc.value = 'juguetón'; */
+
+/* const descrSearchText = inputSearchDesc.value; */
+
+
+/* if (!kittenDesc1.includes(descrSearchText)){
+  document.querySelector('.js_cat1').classList.add('collapsed');
+}
+if (!kittenDesc2.includes(descrSearchText)){
+  document.querySelector('.js_cat2').classList.add('collapsed');
+}
+if (!kittenDesc3.includes(descrSearchText)){
+  document.querySelector('.js_cat3').classList.add('collapsed');
+} */
+
+//Colapsed form
+const menuAdd = document.querySelector ('.js_menuAdd');
+
+menuAdd.addEventListener('click', (event) => {
+  event.preventDefault();
+  const CollapsedForm = document.querySelector('.js-new-form');
+  if (CollapsedForm.classList.contains('collapsed')){
+    CollapsedForm.classList.remove('collapsed');
+    } else {
+      CollapsedForm.classList.add('collapsed');
+    }
+
+})
+
+
+
+
+
+
+
+
+//bonus 3
+/* const raceInput =document.querySelector('.js_race');
+let raceAlert = raceInput.value;
+if (raceAlert==='') {
+  console.log(`No se ha especificado la raza`);
+} else {
+  console.log(raceAlert);
+}; */
+  
